3572fbd2d9a8aa4dced9bd645259e8af1ac943e9
Loic Banet <loic.banet@gmail.com>
2017-11-07 21:14:34 +0100

    doc: add compute

--- pleeplee/compute.py
+++ pleeplee/compute.py
@@ -37,7 +37,7 @@ Before turning on the robot for initialisation phase it has to face a side
 of the perimeter delimited by LEDs. The minimum number of LEDs landmark is 4.
 The LEDs have to be of different colors for the location to work properly.
 
- The map will have the following conventions:
+ The map has the following conventions:
   R(0, 0)                                   G(10, 0)
        _____________________________________
       |                                     |
@@ -81,7 +81,17 @@ PRECISION = 4
 
 
 def getPos2Dist(data1, data2):
+    """Get a set of positions from two sets of datas.
 
+    Based on mathematical computations for the intersections between two circles.
+
+    Args:
+        data1: Data instance.
+        data2: Data instance.
+
+    Returns:
+        A set of points candidates to the location.
+    """
     if data1.distance is None or data2.distance is None:
         raise ValueError('Incomplete datas')
 
@@ -129,9 +139,20 @@ def getPos2Dist(data1, data2):
     return [I1, I2]
 
 
-# remove solutions if they are not whithin the perimeter
 def filterPoints(solutions, corners):
+    """Remove solutions if they are not whithin the perimeter.
 
+    This function use shapely as the mathematical computaions for non rectangular
+    shapes are quite heavy.
+
+    Args:
+        solutions: A list of candidate points.
+
+        corners: The perimeter of the garden (list of LEDs).
+
+    Returns:
+        A list of points filtered.
+    """
     coords = []
     for i in corners:
         if i.inPerimeter:
@@ -146,8 +167,21 @@ def filterPoints(solutions, corners):
 
 
 def isAdjacent(color1, color2, perimeter):
+    """Check if two colors are adjacents in the perimeter.
+
+    The LEDs in the perimeter all have a unique color. This function takes color
+    in parameters to simplify the computations.
+
+    Args:
+        color1: First color.
+        color2: Second color.
+        perimeter: The list of LEDs in the perimeter.
+
+    Returns:
+        True if the two colors are adjacents. False otherwise.
+    """
     if color1 == color2:
-        print("merde")
+        print('Bad datas, not possible')
         return False
     count = 0
     start = False
@@ -162,9 +196,19 @@ def isAdjacent(color1, color2, perimeter):
     return count == 1 or count == len(perimeter)
 
 
-# Get the clockwise vector from two LEDs color in the perimeter
-# the arguments should be given from left to right in the scope of the camera.
 def vectorFromColors(led1, led2, perimeter):
+    """Get the clockwise vector from two LEDs color in the perimeter.
+
+    The arguments should be given from left to right in the scope of the camera.
+
+    Args:
+        led1: The leftmost LED
+        led2: The rightmost LED
+        perimeter: The list of LEDs
+
+    Returns:
+        The vector from led1 to led2
+    """
     if not isAdjacent(led1.color, led2.color, perimeter):
         return led2.point.minus(led1.point)
     count = 0
@@ -185,16 +229,44 @@ def vectorFromColors(led1, led2, perimeter):
             if firstColorInFirst else led1.point.minus(led2.point))
 
 
-# The computation is done with vectors
-# the minus before the angle of rotateVector is necessary because the
-# angles are counter clockwise by convention however the rotation is clockwise
-# in order to correctly compute non adjacent LEDs the order of the input
-# datas is important. They must be in the order as seen by the camera
-# from left to right. This ensure that the robot will always be on the
-# adequate side of the area and the vectPerpendicular calculus will
-# be correct.
 def distanceFromAngles(data1, data2, dirInit, angleNorth, angleToDirection,
                        perimeter):
+    """Distance computation from angle
+
+    This function is the compuation of the distance to the robot given
+    the angle. We cannot do it Data per Data as it is not a computation
+    possible if our only data is the angle. The principle here is to cross
+    two datas and given the position of the two LEDs we have a triangle
+    where we know two angles and a distance. With these datas we can calculate
+    the two remaining distances.
+
+    Implementation details:
+    The computation is done with vectors.
+    The minus before the angle of rotateVector is necessary because the
+    angles are counter clockwise by convention however the rotation is clockwise
+    in order to correctly compute non adjacent LEDs the order of the input
+    datas is important. They must be in the order as seen by the camera
+    from left to right. This ensure that the robot will always be on the
+    adequate side of the area and the vectPerpendicular calculus will
+    be correct.
+
+    Args:
+        data1: The first Data instance.
+
+        data2: The second data instance.
+
+        dirInit: The direction of the robot at initialisation.
+
+        angleNorth: The angle of the robot from the North at initialisation.
+
+        angleToDirection: The angle between the current axis of the robot.
+        and the North
+
+        perimeter: The perimeter of the garden (list of LEDs).
+
+    Returns:
+        A pair of distance (x y) from the robot to respectively (led1 led2).
+    """
 
     vect1 = rotateVector(dirInit, data1.angle)
     vect2 = rotateVector(dirInit, data2.angle)
@@ -209,7 +281,7 @@ def distanceFromAngles(data1, data2, dirInit, angleNorth, angleToDirection,
     if angle1 < angle2:
         angle1, angle2 = angle2, angle1
     distance = abs(data1.led.point.distance(data2.led.point))
-    # if the two angles have different signs their product will de negative
+    # if the two angles have different signs their product will be negative
     if angle1 * angle2 < 0:
         x = distance / (1 + math.tan(math.radians(abs(angle2))) /
                         math.tan(math.radians(abs(angle1))))
@@ -228,8 +300,22 @@ def distanceFromAngles(data1, data2, dirInit, angleNorth, angleToDirection,
         return (x, y)
 
 
-# Final synthetizing of all the datas related to 2 points and computing
 def compute2Data(data1, data2, *args):
+    """Compute a set of position candidates from 2 datas
+
+    Final synthetizing of all the datas related to 2 points and computing.
+
+    Args:
+        data1: first Data instance.
+
+        data2: second Data instance.
+
+        *args: dirInit, angleNorth, angleToDirection, perimeter.
+        See distanceFromAngles.
+
+    Returns:
+        A list of candidate positions for the actual location
+    """
     perimeter = args[-1]
     (dist1, dist2) = distanceFromAngles(data1, data2, *args)
     data1.adjustDistance(dist1)
@@ -239,6 +325,10 @@ def compute2Data(data1, data2, *args):
 
 
 def compute3Data(data1, data2, data3, *args):
+    """Synthetize the position computations for 3 Datas instances.
+
+    See **Compute2Data** for the arguments and return value.
+    """
     res1 = compute2Data(data1, data2, *args)
     res2 = compute2Data(data2, data3, *args)
     res3 = compute2Data(data1, data3, *args)
@@ -247,6 +337,17 @@ def compute3Data(data1, data2, data3, *args):
 
 
 def hasManyOccurencies(elt, listx):
+    """Find if a point has many similar occurences in a list.
+
+    This function is based on a threshold by default it is 80%.
+
+    Args:
+        elt: One element of the list.
+        listx: The list of elements
+
+    Returns:
+        True if it is a very frequent element, False otherwise.
+    """
     _threshold = 80.0
     count = 0
     for i in listx:
@@ -258,6 +359,15 @@ def hasManyOccurencies(elt, listx):
 
 
 def sortData(data_array):
+    """Filter the datas in given in parameter if they appear often.
+
+    Args:
+        data_array: A list of Points candidates to be the actual
+        location of the robot.
+
+    Returns:
+        The mean of the most frequent elements in the list.
+    """
     data_array = [x for x in data_array if hasManyOccurencies(x, data_array)]
     x = 0.0
     y = 0.0
